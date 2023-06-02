8f5c75bc46c83db40bc74fd4b606d93a89e7790d
yosoyroot <wendyfabianpanganiban@gmail.com>
2023-06-02 09:39:17 +0200

    fixed http access

--- AWS/EC2/main.tf
+++ AWS/EC2/main.tf
@@ -63,12 +63,30 @@ resource "aws_security_group" "ssh-access" {
   name        = "ssh-access"
   description = "Allow ssh access from internet"
   ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ 
+  }
+  ingress {
+    description = "ssh"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+ 
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
 # default user is ubuntu
 
 output "publicips" {
